645a2d196787528e2205375684435d17858fe694
hongsifan <hongsifan@bis.com.cn>
2020-01-17 14:50:04 +0800

    test
    Signed-off-by: hongsifan <hongsifan@bis.com.cn>

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -2,5 +2,5 @@ package com.github.nkzawa.socketio.androidchat;
 
 public class Constants {
     //public static final String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
-    public static final String CHAT_SERVER_URL = "http://100.168.1.180:9099";
+    public static final String CHAT_SERVER_URL = "http://jollitycn.com:9099";
 }
